f93682e5eb78b912ba165f1ccdccb66433cad27d
Douglas Ausech <douglas@pennacchi.com.br>
2018-04-24 06:22:08 -0300

    iniciando app manager

---
+++ manager/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyBBO9TuiHZv7vcVIVLr_rdsXAC9V_Vwo5c',
+            authDomain: 'manager-cbb4d.firebaseapp.com',
+            databaseURL: 'https://manager-cbb4d.firebaseio.com',
+            projectId: 'manager-cbb4d',
+            storageBucket: 'manager-cbb4d.appspot.com',
+            messagingSenderId: '211035834790'
+        };
+        firebase.initializeApp(config);
+    }
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>Hello!</Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
