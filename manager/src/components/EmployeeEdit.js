a3441c1bfb091e59af8b863a051cfdfb8d2d6ee1
Douglas Ausech <dausech@gmail.com>
2018-05-12 07:25:19 -0300

    Gravando alteracoes no firebase

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import Communications from 'react-native-communications';
+import { employeeUpdate, employeeSave } from '../actions';
 import EmployeeForm from './EmployeeForm';
 import { Card, CardSection, Button } from './common';
+import { ECONNABORTED } from 'constants';
 
 class EmployeeEdit extends Component {
 
@@ -16,16 +18,29 @@ class EmployeeEdit extends Component {
     onButtonPress() {
         const { name, phone, shift } = this.props;
 
-        console.log(name, phone, shift);
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+    }
+
+    onTextPress() {
+        const { phone, shift } = this.props;
+
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
     }
 
     render() {
         return (
             <Card>
                 <EmployeeForm  />
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Save Changes
+                    </Button>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>
+                        Text Schedule
                     </Button>
                 </CardSection>
             </Card>
@@ -37,4 +52,4 @@ const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
     return { name, phone, shift };
 }
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
